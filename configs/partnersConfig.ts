bea56098d71da3475c6601bfe806d04e4599237f
Dodge (Lang HE) <asdsay@gmail.com>
2025-02-24 15:09:42 +0800

    fix partners images

--- configs/partnersConfig.ts
+++ configs/partnersConfig.ts
@@ -9,27 +9,27 @@ const partnersConfig: Config = {
   partnerTitle: "Our Partners & Backers",
   partnersImg: [
     {
-      imageSrcPath: "images/homepage/partners_logo1.png",
+      imageSrcPath: "images/partners/partners_logo1.png",
       title: "Partner 1",
       content: "",
     },
     {
-      imageSrcPath: "/images/homepage/partners_logo2.png",
+      imageSrcPath: "/images/partners/partners_logo2.png",
       title: "Partner 2",
       content: "",
     },
     {
-      imageSrcPath: "/images/homepage/partners_logo3.png",
+      imageSrcPath: "/images/partners/partners_logo3.png",
       title: "Partner 3",
       content: "",
     },
     {
-      imageSrcPath: "/images/homepage/partners_logo4.png",
+      imageSrcPath: "/images/partners/partners_logo4.png",
       title: "Partner 4",
       content: "",
     },
     {
-      imageSrcPath: "/images/homepage/partners_logo5.png",
+      imageSrcPath: "/images/partners/partners_logo5.png",
       title: "Partner 5",
       content: "",
     },
